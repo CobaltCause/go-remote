2d5ae720eded7b94b86127b61b108aeeab0cab2c
Charles Hall <charles@computer.surgery>
2025-08-27 22:13:54 -0700

    implement Start/Stop/Status

---
+++ pkg/process_manager_test.go
@@ -0,0 +1,60 @@
+package process_manager
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStartStatusStopStatus(t *testing.T) {
+	pm := new(ProcessManager)
+
+	var wg sync.WaitGroup
+
+	// TODO: Use wg.Go when 1.25 is available in nixpkgs.
+	for range 3 {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			innerStartStatusStopStatus(t, pm)
+		}()
+	}
+
+	wg.Wait()
+}
+
+func innerStartStatusStopStatus(t *testing.T, pm *ProcessManager) {
+	assert := assert.New(t)
+
+	id, err := pm.Start("sleep", "sleep", "infinity")
+	if !assert.Nil(err) {
+		return
+	}
+
+	st, err := pm.Status(id)
+	if !assert.Nil(err) {
+		return
+	}
+	assert.Equal(RUNNING, st.State, "process should be running")
+
+	// Do this to try ensure repeated/concurrent calls to `Stop` for the same
+	// process ID don't cause problems.
+	//
+	// TODO: Use wg.Go when 1.25 is available in nixpkgs.
+	var wg sync.WaitGroup
+	for range 3 {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			assert.Nil(pm.Stop(id))
+		}()
+	}
+	wg.Wait()
+
+	st, err = pm.Status(id)
+	if !assert.Nil(err) {
+		return
+	}
+	assert.Equal(STOPPED, st.State, "process should be stopped")
+}

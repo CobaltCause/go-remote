b6062daa077fbf7e73499b9caa33d6d87c803c27
Charles Hall <charles@computer.surgery>
2025-08-27 22:31:06 -0700

    change process ID type
    `len(p)` in Go returns `int` (odd that it doesn't use an unsigned
    integer type), so switching to use `int` for process IDs will be less of
    an impedance mismatch with how I plan to issue process IDs.
    
    I'll continue to use `uint32` on the ProtoBuf side though. Ideally,
    I'd add a `(ProcessManager).Len` method that can be used to ensure new
    processes won't have an ID outside the bounds of `uint32` but I don't
    think that's really necessary for this project. Also, that would require
    spawning a ton of processes.

--- pkg/process_manager.go
+++ pkg/process_manager.go
@@ -35,7 +35,7 @@ type ProcessManager struct {
 	// TODO
 }
 
-func (*ProcessManager) Start() uint32 {
+func (*ProcessManager) Start() int {
 	panic("TODO")
 }
 
@@ -43,21 +43,21 @@ func (*ProcessManager) Start() uint32 {
 //
 // If the error is not `nil`, then the process was stopped. An error is returned
 // if the process ID does not exist.
-func (*ProcessManager) Stop(id uint32) error {
+func (*ProcessManager) Stop(id int) error {
 	panic("TODO")
 }
 
 // Status returns the status of a process by its ID.
 //
 // An error is returned if the process ID is not known.
-func (*ProcessManager) Status(id uint32) (ProcessStatus, error) {
+func (*ProcessManager) Status(id int) (ProcessStatus, error) {
 	panic("TODO")
 }
 
 // Stream gets a reader of the process's stdout and stderr since it was started.
 //
 // Returns `nil, nil` if the process ID is not known.
-func (*ProcessManager) Stream(id uint32) (stdout io.Reader, stderr io.Reader) {
+func (*ProcessManager) Stream(id int) (stdout io.Reader, stderr io.Reader) {
 	// Would construct a `processStream` for stderr and stdout and return it.
 	panic("TODO")
 }

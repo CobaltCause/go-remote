77ead22f3b99d7fd7315a0c31e087d472d3a01ea
Charles Hall <charles@computer.surgery>
2025-09-01 14:10:34 -0700

    implement (*ProcessManager).Stream

--- pkg/process_manager.go
+++ pkg/process_manager.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os/exec"
 	"sync"
+	"sync/atomic"
 	"syscall"
 )
 
@@ -31,12 +32,142 @@ type ProcessStatus struct {
 	State    ProcessState
 }
 
-type processStream struct {
-	// TODO
+// sharedBufWriter is the write half of a shared buffer that allows multiple
+// readers.
+type sharedBufWriter struct {
+	bufLock sync.RWMutex
+	buf     []byte
+
+	closed atomic.Bool
+
+	readersLock sync.Mutex
+	readers     map[chan struct{}]struct{}
 }
 
-func (s *processStream) Read(p []byte) (n int, err error) {
-	panic("TODO")
+func (s *sharedBufWriter) Write(p []byte) (n int, err error) {
+	if len(p) == 0 {
+		return 0, nil
+	}
+
+	func() {
+		s.bufLock.Lock()
+		defer s.bufLock.Unlock()
+		s.buf = append(s.buf, p...)
+	}()
+
+	s.readersLock.Lock()
+	defer s.readersLock.Unlock()
+	for output := range s.readers {
+		// Non-blocking send.
+		select {
+		case output <- struct{}{}:
+		default:
+		}
+	}
+
+	return len(p), nil
+}
+
+// Close informs all associated readers (including readers that haven't been
+// created yet) that the buffer will no longer receive new data.
+//
+// This method always returns a `nil` error.
+func (s *sharedBufWriter) Close() error {
+	s.readersLock.Lock()
+	defer s.readersLock.Unlock()
+	for output := range s.readers {
+		close(output)
+	}
+
+	s.closed.Store(true)
+
+	return nil
+}
+
+func (s *sharedBufWriter) newReader() *sharedBufReader {
+	s.readersLock.Lock()
+	defer s.readersLock.Unlock()
+
+	// The writer needs to be able to buffer at least one notification to avoid
+	// allowing Read to block waiting for new writes when it shouldn't.
+	output := make(chan struct{}, 1)
+
+	// If the writer is closed, immediately close the channel to indicate that
+	// there will never be new data to wait for.
+	if s.closed.Load() {
+		close(output)
+	}
+
+	s.readers[output] = struct{}{}
+	return &sharedBufReader{
+		writer: s,
+		output: output,
+	}
+}
+
+// sharedBufReader is the read half of a shared buffer that allows multiple
+// readers.
+type sharedBufReader struct {
+	writer *sharedBufWriter
+
+	// pos tracks how far into buf has been read already.
+	pos int
+
+	// caughtUp indicates whether Read has read up to the end of buf at least
+	// once.
+	caughtUp bool
+
+	// output can be received from to wait for new data to be written by the
+	// writer.
+	output chan struct{}
+}
+
+func (s *sharedBufReader) Read(p []byte) (n int, err error) {
+	if len(p) == 0 {
+		return 0, nil
+	}
+
+	// Whether the channel is still open.
+	open := true
+	select {
+	case _, open = <-s.output:
+	default:
+		// Wait for new data if the channel is open and we're caught up.
+		if s.caughtUp {
+			<-s.output
+		}
+	}
+
+	func() {
+		s.writer.bufLock.RLock()
+		defer s.writer.bufLock.RUnlock()
+		n = copy(p, s.writer.buf[s.pos:])
+	}()
+
+	if n == 0 {
+		s.caughtUp = true
+		if open {
+			// Try again to wait for more data.
+			return s.Read(p)
+		} else {
+			// There won't be any more data, this is the end of the buffer.
+			return 0, io.EOF
+		}
+	}
+	s.pos += n
+
+	return n, nil
+}
+
+// Close unregisters this reader with its writer, freeing resources.
+//
+// This method always returns a `nil` error.
+func (s *sharedBufReader) Close() error {
+	s.writer.readersLock.Lock()
+	defer s.writer.readersLock.Unlock()
+	delete(s.writer.readers, s.output)
+
+	return nil
 }
 
 type process struct {
@@ -44,6 +175,8 @@ type process struct {
 	statusLock   sync.RWMutex
 	status       ProcessStatus
 	terminatedCh chan struct{}
+	stdout       *sharedBufWriter
+	stderr       *sharedBufWriter
 }
 
 func (s *process) markTerminated() {
@@ -80,8 +213,13 @@ func (s *ProcessManager) acquireProcess(id int) (*process, error) {
 // This function fails if starting the process fails.
 func (s *ProcessManager) Start(path string, args ...string) (int, error) {
 	ctx, cancel := context.WithCancel(context.Background())
+	stdout := &sharedBufWriter{readers: make(map[chan struct{}]struct{})}
+	stderr := &sharedBufWriter{readers: make(map[chan struct{}]struct{})}
+
 	cmd := exec.CommandContext(ctx, path)
 	cmd.Args = args
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
 
 	if err := cmd.Start(); err != nil {
 		cancel()
@@ -91,6 +229,8 @@ func (s *ProcessManager) Start(path string, args ...string) (int, error) {
 	p := &process{
 		cancel:       cancel,
 		terminatedCh: make(chan struct{}),
+		stdout:       stdout,
+		stderr:       stderr,
 	}
 
 	go func() {
@@ -99,6 +239,13 @@ func (s *ProcessManager) Start(path string, args ...string) (int, error) {
 		_, ok := err.(*exec.ExitError)
 		if err != nil && !ok {
 			log.Print("failed to wait for process: ", err)
+		}
+
+		if stdout.Close() != nil {
+			panic("unreachable")
+		}
+		if stderr.Close() != nil {
+			panic("unreachable")
 		}
 
 		processStatus := ProcessStatus{}
@@ -196,7 +343,14 @@ func (s *ProcessManager) Status(id int) (ProcessStatus, error) {
 // Stream gets a reader of the process's stdout and stderr since it was started.
 //
 // Returns `nil, nil` if the process ID is not known.
-func (s *ProcessManager) Stream(id int) (stdout io.Reader, stderr io.Reader) {
-	// Would construct a `processStream` for stderr and stdout and return it.
-	panic("TODO")
+func (s *ProcessManager) Stream(id int) (
+	stdout io.ReadCloser,
+	stderr io.ReadCloser,
+) {
+	p, err := s.acquireProcess(id)
+	if err != nil {
+		return nil, nil
+	}
+
+	return p.stdout.newReader(), p.stderr.newReader()
 }
